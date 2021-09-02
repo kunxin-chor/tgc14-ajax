7fc6556f943dc69e18eb716df6a84b88d76f3919
kunxin-chor <chorkunxin@yahoo.com>
2021-09-02 07:14:44 +0000

    Added book search solution

--- hands-on/q8-random-users/script.js
+++ hands-on/q8-random-users/script.js
@@ -1,1 +1,93 @@
-let url = "https://randomuser.me/api/?results=10"
+
+// window.addEventListener('DOMContentLoaded', function(){
+//     let url = "https://randomuser.me/api/?results=10";
+
+//     // select the button and make it run some code when it is clicked
+//     document.querySelector('#load-btn').addEventListener('click', async function(){
+             
+//         // remove all existing child elements in #users
+//         document.querySelector('#users').innerHTML = "";
+       
+//         let response = await axios.get(url);
+//         for (let eachUser of response.data.results) {
+           
+//             let name = eachUser.name.first + " " + eachUser.name.last;
+//             let city = eachUser.location.city;
+//             let image = eachUser.picture.medium
+       
+
+//             /*
+//             <div class="user">
+//                 <h1>Tan Ah Kow</h1>
+//                 <h2>City: Singapore</h2>
+//                 <img src="ahkow.jpg"/>
+//             </div>
+//             */
+
+//             // use append child
+//             let divElement = document.createElement('div');
+//             divElement.className = "user";
+
+//             let h1Element = document.createElement('h1');
+//             h1Element.innerHTML = name;
+//             // add h1Element as a child of divElement
+//             divElement.appendChild(h1Element);
+
+//             let h2Element = document.createElement('h2');
+//             h2Element.innerHTML = "City: " + city;
+//             divElement.appendChild(h2Element);
+
+//             let imageElement = document.createElement('img');
+//             imageElement.src = image; 
+//             divElement.appendChild(imageElement);
+
+//             // use append child and innerHTML
+//             document.querySelector('#users').appendChild(divElement);
+
+//         }
+//     })
+// });
+
+
+window.addEventListener('DOMContentLoaded', function(){
+    let url = "https://randomuser.me/api/?results=10";
+
+    // select the button and make it run some code when it is clicked
+    document.querySelector('#load-btn').addEventListener('click', async function(){
+             
+        // remove all existing child elements in #users
+        document.querySelector('#users').innerHTML = "";
+       
+        let response = await axios.get(url);
+        for (let eachUser of response.data.results) {
+           
+            let name = eachUser.name.first + " " + eachUser.name.last;
+            let city = eachUser.location.city;
+            let image = eachUser.picture.medium
+       
+
+            /*
+            <div class="user">
+                <h1>Tan Ah Kow</h1>
+                <h2>City: Singapore</h2>
+                <img src="ahkow.jpg"/>
+            </div>
+            */
+
+            // use append child
+            let divElement = document.createElement('div');
+            divElement.className = "user";
+
+            divElement.innerHTML = `
+            <div class="user">
+                <h1>${name}</h1>
+                <h2>City: ${city}</h2>
+                <img src="${image}"/>
+            </div>
+            `
+            // use append child and innerHTML
+            document.querySelector('#users').appendChild(divElement);
+
+        }
+    })
+});
---
+++ hands-on/q9-book-search/script.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', function(){
+    // using const informs other programmers that the variable API_BASE_URL
+    // shouldn't be reassigned
+    const BASE_API_URL = 'https://openlibrary.org';
+    document.querySelector('#search-btn').addEventListener('click', async function(){
+        let searchTerms = document.querySelector('#search-terms').value;
+        /*
+            let searchTermElement = document.querySelector("#search-terms");
+            let searchTerms = searchTermElement.value;
+        */
+
+        // let response = await axios.get(BASE_API_URL+"/search.json?q=The Lord of the Rings&limit=10");
+        let response = await axios.get(BASE_API_URL+"/search.json",{
+            'params': {
+                'title':searchTerms,
+                'limit': 10
+            }
+        })
+        let resultDiv =  document.querySelector('#results');
+        resultDiv.innerHTML = "";
+        for (let book of response.data.docs) {
+            let html = `
+                <li>${book.title} by ${book.author_name.join(", ")}</li>
+            `
+           resultDiv.innerHTML += html;
+        }
+
+    }) 
+})
