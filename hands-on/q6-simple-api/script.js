02fb16117ea455bc6e7fbe3a461560b756f962a9
kunxin-chor <chorkunxin@yahoo.com>
2021-09-02 03:01:35 +0000

    Added soln to hands-on 1

--- hands-on/q6-simple-api/script.js
+++ hands-on/q6-simple-api/script.js
@@ -1,2 +1,16 @@
 
-let url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+let url = "https://api.kanye.rest/"
+
+// 
+window.addEventListener('DOMContentLoaded', function () {
+    let btn = document.querySelector('#random-btn');
+    document.querySelector('#random-btn').addEventListener('click', async function () {
+        let response = await axios.get(url);
+        console.log(response.data.quote);
+        let quote = response.data.quote;
+        document.querySelector('#quote').innerHTML = quote;
+
+    })
+})
+
+
